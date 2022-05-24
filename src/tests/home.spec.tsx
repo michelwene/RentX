347918a7faa2551f69925260521e2e86560189aa
Michel Wene <michelwene@hotmail.com>
2022-05-23 21:14:47 -0300

    test: Page Home

--- src/tests/home.spec.tsx
+++ src/tests/home.spec.tsx
@@ -3,3 +3,25 @@
  */
 import { screen, render } from "@testing-library/react";
 import Home from "../pages/index";
+
+describe("page Home", () => {
+  it("should render component Home", () => {
+    render(<Home />);
+
+    expect(
+      screen.getByText("Alugue um carro de maneira simples e fácil")
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByText(
+        "Vários modelos para você dirigir seguro, com conforto e segurança."
+      )
+    ).toBeInTheDocument();
+  });
+
+  it("should show the text Button", () => {
+    render(<Home />);
+
+    expect(screen.getByText("Começar agora")).toBeInTheDocument();
+  });
+});
